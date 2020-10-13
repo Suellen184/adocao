cb40a62816b893e7b1c476e5dda7f16217becb59
Suellen184 <suellencrisvieira184@gmail.com>
2020-10-13 19:49:27 -0300

    atualizacao layout create animal

--- resources/views/admin/animal/create.blade.php
+++ resources/views/admin/animal/create.blade.php
@@ -17,65 +17,72 @@
 
 <form action="{{route('admin.animal.store')}}" method="post" autocomplete="off" enctype="multipart/form-data">
     @csrf
-    
-    <div class="form-group">
+
+  <!--  <div style="border:  2px dashed rgb(151, 151, 151);border-radius: 8px; text-align: center; width: 200px; height: 200px;">    <label name="imagem" for="avatar_loader-c15e-36af-a130" style="font-size: 12px; text-align: center; font-weight: 700; color: black; display: inline-block; font-family: sans-serif; cursor: pointer; line-height: 300px;">Insira uma imagem</label></div>
+  -->
+  <input type="file" name="imagem" id="imagem">
+    <div class="row">
+    <div class="form-group col-sm-8">
       <label for="nome">Nome</label>
-      <input type="text" class="form-control col-sm-6" id="nome" name="nome" aria-describediby="Nome" placeholder="Nome do animal">
+      <input type="text" class="form-control " id="nome" name="nome" aria-describediby="Nome" placeholder="Nome do animal">
     </div>
 
-    <div class="form-group">
+    <div class="form-group col-sm-4">
         <label for="cor">Cor</label>
-        <input type="text" class="form-control col-sm-4" id="cor" name="cor" placeholder="Cor do animal">
+        <input type="text" class="form-control" id="cor" name="cor" placeholder="Cor do animal">
     </div>
-
-    <div class="form-group">
+</div>    
+<div class="row">
+    <div class="form-group  col-sm-4">
         <label for="raca">Raça</label>
-        <input type="text" class="form-control col-sm-4" id="raca" name="raca" placeholder="Raça do animal">
+        <input type="text" class="form-control" id="raca" name="raca" placeholder="Raça do animal">
     </div>
 
-
-    <div class="form-group">
+    <div class="form-group col-sm-4">
         <label for="nascimento">Data de Nascimento</label>
-        <input type="date" class="form-control col-sm-4" id="nascimento" name="nascimento" placeholder="Data de Nascimento">
+        <input type="date" class="form-control" id="nascimento" name="nascimento" placeholder="Data de Nascimento">
     </div>
 
-    <div class="form-group">
+    <div class="form-group col-sm-4">
       <label for="status">Tamanho</label>
-      <select class="form-control col-sm-2"  name="tamanho"  id="tamanho">
+      <select class="form-control"  name="tamanho"  id="tamanho">
         <option value="pequeno">Pequeno</option>
         <option value="medio">Médio</option>
         <option value="grande">Grande</option>
       </select>
   </div>
-
-    <div class="form-group">
+</div>
+<div class="row">
+    <div class="form-group col-sm-4">
         <label for="tipo">Tipo</label>
-        <select class="form-control col-sm-2" name="tipo" id="tipo">
+        <select class="form-control" name="tipo" id="tipo">
           <option value="cachorro">Cachorro</option>
           <option value="gato">Gato</option>
         </select>
     </div>
 
-    <div class="form-group">
+    <div class="form-group col-sm-4">
         <label for="sexo">Sexo</label>
-        <select class="form-control col-sm-2"  name="sexo" id="sexo">
+        <select class="form-control"  name="sexo" id="sexo">
           <option value="femea">Fêmea</option>
           <option value="macho">Macho</option>
         </select>
     </div>
-    <div class="form-group">
+    <div class="form-group col-sm-4">
         <label for="status">Status</label>
-        <select class="form-control col-sm-2"  name="status"  id="status">
+        <select class="form-control"  name="status"  id="status">
           <option value="inativo">Inativo</option>
           <option value="ativo">Ativo</option>
         </select>
     </div>
 
-    <div class="form-group">
-    <label for="exampleFormControlTextarea1">Observação Administrador</label>
-    <textarea class="form-control" name="observacao" id="exampleFormControlTextarea1" rows="3"></textarea>
   </div>
-  <input type="file" class="form-control col-sm-6" id="imagem" name="imagem">
+
+    <div class="form-group col-sm-12">
+    <label for="exampleFormControlTextarea1">Observação Administrador</label>
+    <textarea class="form-control " name="observacao" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+
 
     <button type="submit" class="btn btn-success">Enviar</button>
   </form>
